40cfe3cd3eadba266cd4d1ffce1acaa21da979ab
Maciek Nowacki <nowmaciek@icloud.com>
2021-12-04 13:07:18 -0700

    part two with, sigh, 1-based indexing

--- day_2/how_many_passwords_are_valid.rb
+++ day_2/how_many_passwords_are_valid.rb
@@ -5,6 +5,12 @@ matches = ARGF.map do |line|
     [(s.to_i..e.to_i), l, pwd]
 end
 
+puts 'part one:'
 puts (matches.count do |range, char, pwd|
     range.include?(pwd.count(char))
+end)
+
+puts 'part two:'
+puts (matches.count do |range, char, pwd|
+    1 == pwd.chars.values_at(range.begin.pred, range.end.pred).count(char)
 end)
