76396f874eee1a826d3502d11e00ad5c6e40eae8
Maciek Nowacki <nowmaciek@icloud.com>
2021-12-04 12:24:08 -0700

    general solution for both parts

--- day_1/find_pair_summing_to_2020.rb
+++ day_1/find_pair_summing_to_2020.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/env ruby
 
-puts (ARGF.map(&:to_i).combination(3) do |comb|
-    break comb.reduce(:*) if comb.sum == 2020
-end)
+numbers = ARGF.map(&:to_i)
+
+puts ((2..3).map do |pick_n|
+    numbers.combination(pick_n) do |comb|
+        break "found #{pick_n} numbers that sum to 2020 and multiply to #{comb.reduce(:*)}" if comb.sum == 2020
+    end
+end.join("\n"))
